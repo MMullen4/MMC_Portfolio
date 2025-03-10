d732b8a8b3a15d23b9b9941e48bb606cf2869af9
Matt Mullen <mhmullen@icloud.com>
2025-03-10 16:17:21 -0700

    added files / structure for components

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client' // allows us to use the DOM
+import App from './App' // allows App component to be used
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
